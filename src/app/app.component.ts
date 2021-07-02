9e165e8fbb13dacc8e182414b7c42504b3f6ff69
Daniel <daniel.i.d.ch@gmail.com>
2021-07-01 21:50:02 -0600

    mole

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mole';
+  isShow = true;
+  isShow2 = true;
+  isShow3 = true;
+  isShow4 = true;
+  isShow5 = true;
+  isShow6 = true;
+
+  toggleDisplay() {
+    this.isShow = !this.isShow;
+  }
+  toggleDisplay2() {
+    this.isShow2 = !this.isShow2;
+  }
+  toggleDisplay3() {
+    this.isShow3 = !this.isShow3;
+  }
+  toggleDisplay4() {
+    this.isShow4 = !this.isShow4;
+  }
+  toggleDisplay5() {
+    this.isShow5 = !this.isShow5;
+  }
+  toggleDisplay6() {
+    this.isShow6 = !this.isShow6;
+  }
 }
